45f8f096fb5112b64b794168a61d88e5ffa00c0c
SIyuyuko <3228981717@qq.com>
2024-04-25 12:02:26 +0800

    调整异步请求谱面逻辑

--- src/.vuepress/utils/mappool.js
+++ src/.vuepress/utils/mappool.js
@@ -6,30 +6,41 @@ import { getBeatmapInfo } from '@dataApi';
 * @param {String} pool 图池对象
 * @return void
 */
-async function getMapInfo(poolList,poolName) {
+function getMapInfo(poolList, poolName) {
 	for (let pool in poolList) {
-		let sets=poolList[pool].sets;
-  		let poolData=poolList[pool].data;
-  		for(let item of sets){
-    		await getBeatmapInfo(item.id).then((res)=>{
-      			if(res.status===200 && res.data){
-        			let data=res.data;
-					let tag = item.tag;
-					let last = false;
-        			let obj=Object.assign(data,{tag:tag,isLast:last});
-        			poolData.push(obj);
-        			if(item===sets[sets.length-1]){
-						poolList[pool].status.isLoading = false;
-						isLastMap(poolData);
-        			};
-					} else {
-						alert("谱面加载失败，请刷新页面重新加载。")
-				};
-      		});
-		};
+		beatmapsetRequest(poolList, pool);
 	};
-	localStorage.setItem(poolName,JSON.stringify(poolList));
+	localStorage.setItem(poolName, JSON.stringify(poolList));
 };
+/**
+* @description 异步请求谱面
+* @param {Object} poolList 图池列表对象
+* @param {String} pool 图池对象
+* @return void
+*/
+async function beatmapsetRequest(poolList, pool) {
+	/** @param {Array} sets 图池谱面ID集合 */
+	let sets = poolList[pool].sets;
+	/** @param {Object} poolData 图池谱面信息集合 */
+	let poolData = poolList[pool].data;
+	for (let item of sets) {
+		await getBeatmapInfo(item.id).then((res) => {
+			if (res.status === 200 && res.data) {
+				let data = res.data;
+				let tag = item.tag;
+				let last = false;
+				let obj = Object.assign(data, { tag: tag, isLast: last });
+				poolData.push(obj);
+				if (item === sets[sets.length - 1]) {
+					poolList[pool].status.isLoading = false;
+					isLastMap(poolData);
+				};
+			} else {
+				alert("谱面加载失败，请刷新页面重新加载。")
+			};
+		});
+	};
+}
 /**
 * @description 处理比赛图池原数据
 * @param {Object} pool 图池对象
@@ -42,7 +53,7 @@ function splitPoolString(pool) {
 	let arr = str.split(' ');
 	let sets = pool.sets;
 	let obj = {};
-    let tag;
+	let tag;
 	for (let i in arr) {
 		if (isNaN(parseInt(arr[i]))) {
 			tag = arr[i];
@@ -65,10 +76,10 @@ function splitPoolString(pool) {
 function getMappoolPanel(poolList, poolName) {
 	let list = JSON.parse(localStorage.getItem(poolName));
 	if (!list) {
-		for(let pool in poolList){
+		for (let pool in poolList) {
 			splitPoolString(poolList[pool]);
 		}
-		getMapInfo(poolList,poolName);
+		getMapInfo(poolList, poolName);
 	} else {
 		poolList = list;
 	}
@@ -83,23 +94,23 @@ function getMappoolPanel(poolList, poolName) {
 function isLastMap(poolData) {
 	let data = poolData;
 	let index = 0;
-	for (let i = 0; i < data.length;i++) {
-		for (let j = 1; j < data.length; j++){
-			if(j === i + 1) {
+	for (let i = 0; i < data.length; i++) {
+		for (let j = 1; j < data.length; j++) {
+			if (j === i + 1) {
 				if (data[i].tag !== data[j].tag) {
-			  	let arr = data.slice(index, i+1);
-				index = i+1;
+					let arr = data.slice(index, i + 1);
+					index = i + 1;
 					if (arr.length % 2 !== 0) {
 						arr.map((e) => {
 							if (e === arr[arr.length - 1]) {
 								e.isLast = true;
-						}
-					})
+							}
+						})
+					}
 				}
-			}
 			}
 		}
 		data[data.length - 1].isLast = true;
 	}
 }
-export { getMapInfo,splitPoolString,getMappoolPanel };
+export { getMapInfo, splitPoolString, getMappoolPanel };
