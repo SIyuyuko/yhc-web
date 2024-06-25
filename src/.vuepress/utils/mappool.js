85e1ee5aa9bb2e94520bfb6edf9154e7de7d3e6e
SIyuyuko <3228981717@qq.com>
2024-06-25 15:56:34 +0800

    modified:   src/.vuepress/utils/mappool.js

--- src/.vuepress/utils/mappool.js
+++ src/.vuepress/utils/mappool.js
@@ -192,21 +192,15 @@ function downloadJsonFile(poolList, poolName) {
 async function loadJson(poolList, poolName, flag) {
 	// 图池对象为ref以便更新数据
 	// 动态引入文件路径判断文件是否存在
-	let filepath = window.origin.includes("github") ? `./js/mappool/${poolName}.json` : `../public/js/mappool/${poolName}.json`;
-	const file = import(/* @vite-ignore */filepath);
-	// 文件不存在时，请求数据生成json
-	file.then().catch((e) => {
+	let filepath = window.origin.includes("github") ? `./js/mappool/${poolName}.json` : `../public/assets/js/mappool/${poolName}.json`;
+	await import(/* @vite-ignore */filepath).then((res) => {
+		// 文件存在时，读取文件json
+		poolList.value = res.default;
+		flag.value = false;
+	}).catch((e) => {
+		// 文件不存在时，请求数据生成json
 		poolList.value = getMappoolPanel(poolList.value, poolName);
 		flag.value = true;
 	});
-	// 文件存在时，读取文件json
-	if (file.status !== "rejected") {
-		await file.then((res) => {
-			nextTick(() => {
-				poolList.value = res.default;
-				flag.value = false;
-			})
-		});
-	}
 }
 export { getMapInfo, splitPoolString, getMappoolPanel, getModDiffStar, downloadJsonFile, loadJson };
